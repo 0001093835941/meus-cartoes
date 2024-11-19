81c767226cf60a176d59096149098d4c70e7d385
0001093835941 <00001093835941sp@aluno.educacao.sp.gov.br>
2024-11-19 10:41:43 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'biotecnologia',
+    'qual o papel da biotecnologia na prudução de vacinas?',
+    'desenvolver vacinas mais eficazes e seguras'
+)
+
+criaCartao(
+    'roboticaa',
+    'Qual é o principal benefício dos robôs industriais?',
+    'aumentar eficiência e produtividade'
+)
+
+criaCartao(
+    'empreendedorismo',
+    'qual o principal fator para o sucesso de um empreendedor?',
+    'visão, resiliência, inovação, e capacidade de se adaptar a mudanças'
+)
+
+criaCartao(
+    'história',
+    'o que marcou o fim da segunda guerra mundial?',
+    'rendição do japão em 2 de setembro de 1945'
+)
